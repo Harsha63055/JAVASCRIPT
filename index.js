fb96c5cf10158018e2c50d8b946b5851c91dd8d8
HarshaVardhan Developer <harshavardhan96310@gmail.com>
2024-12-19 10:05:06 +0530

    files

---
+++ index.js
@@ -0,0 +1,81 @@
+// let a=12;
+// let b=14;
+// let c=a+b;
+// console.log(c);
+
+// let obj={
+//     name:"Harsha",
+//     age:25,
+//     city:"Bangalore"
+// }
+// let senten=obj.name + obj.age;
+// console.log(senten)
+
+// let x=5;
+// let y=x--;
+// console.log(y++)
+// let x=24;
+// let y=12;
+// let z= --x + --y ;
+// console.log(z)
+
+// let x = 10;
+// x+= 5; // we can use x = x+5;
+// console.log(x);
+
+// let x1=11;
+// let x2=11;
+// console.log(x1!=x2)
+// console.log(x1===x2)
+// console.log(x1==x2)
+// console.log(x1!==x2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
