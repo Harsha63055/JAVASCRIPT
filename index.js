fd5e474afadf76286d04d75c3c6813f9e1dd97b3
HarshaVardhan Developer <harshavardhan96310@gmail.com>
2024-12-24 09:42:09 +0530

    files

--- index.js
+++ index.js
@@ -21,55 +21,54 @@
 
 // let x = 10;
 // x+= 5; // we can use x = x+5;
-// console.log(x);
+// console.(log(x);
+
+// arr.forEach((name, index) => {
+//     console.log(`${index + 1}.${name}`);
+//   });
 
 // let x1=11;
 // let x2=11;
 // console.log(x1!=x2)
 // console.log(x1===x2)
 // console.log(x1==x2)
-// console.log(x1!==x2)
+// ( console.log(x1!==x2)
 
+// debugger;
 
+// var a = 12;
+// let b = 13;
+// console.log(a);
+// console.log(b);
 
+// let arr = ["hi", "hello", "js"];
+// for (let i in arr) {
+//   console.log("Hello","hiii");
+// }
 
+// let str="javascript";
+// for(i=0; i<10; i++){
+//     console.log(str[i]);
+// }
 
+// let arr = ["mahesh", "somesh", "suresh", "naresh"];
+// for(var i=0; "-", i++){
+//     // console.log(arr)
+//     console.log(arr[i]);
+// }
 
+// let arr = ["mahesh", "somesh", "suresh", "naresh"];
 
+// console.log("1-" + arr[0]);
+// console.log("2-" + arr[1]);
+// console.log("3-" + arr[2]);
+// console.log("4-" + arr[3]);
+// console.log(1+10); // undefined
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for(i=1;1<=20;i++);
+   {
+    console.log("wecome",i);
+}
 
 
 
